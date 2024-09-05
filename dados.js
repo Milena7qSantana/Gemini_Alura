f466ef261f983977956f7716e09b6641478c0bd0
Dennis Matos <milena.msanta@gmail.com>
2024-09-04 21:25:11 -0300

    Adição de sugestões e objeto no JavaScript

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+  {
+    Nome: "Solaire de Astora",
+    descricao:
+      "Solaire de Astora é um dos personagens mais memoráveis de Dark Souls. Cavaleiro de Astora, busca incansavelmente seu próprio sol, espalhando otimismo e camaradagem em um mundo sombrio e cruel. Sua jornada se cruza com a do jogador, oferecendo ajuda e companheirismo, mas também revelando um lado mais profundo e complexo.",
+    link: "https://darksouls.wiki.fextralife.com/Solaire+of+Astora",
+  },
+  {
+    Nome: "Siegmeyer de Catarina",
+    descricao:
+      "Um personagem em Dark Souls.O personagem foi revelado no anúncio do produto. Um cavaleiro jurando lealdade a uma ordem não identificada de guerreiros, este NPC pode ocasionalmente ajudar o jogador em vários pontos do jogo.",
+    link: "https://darksouls.wiki.fextralife.com/Solaire+of+Astora",
+  },
+  {
+    Nome: "Andre de Astora",
+    descricao:
+      "Andre of Astora é um personagem em Dark Souls. Ele está localizado no porão da antiga igreja, um edifício em desuso que conecta a Paróquia dos Mortos Livres com o Jardim da Pedra Negra. Ele vai reparar e atualizar suas armas e armaduras para uma taxa de almas e materiais. Ele também pode ascender a certos armamentos se for dada a brasa adequada.",
+    link: "https://darksouls.wiki.fextralife.com/Solaire+of+Astora",
+  },
+];
--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-console.log("Olá mundo!");
+console.log(dados[1].descricao);
